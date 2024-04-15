732bead29807add767559c9844e24495cc21a7b8
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2024-04-15 09:02:58 +0530

    commit

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+"use strict";
+const fs = require('fs');
+const text=fs.readFileSync('test.txt','utf-8');
+console.log(text);
+//Local variable//
+//class//
+// const pop = document.querySelector(".Pop");
+// const queryNameList = document.querySelector(".Query_Name_List");
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+// //function//
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+// //Load Data//
+// function loadDataJson() {
+//   fetch("/Tranning/heyconvo_tranning.json") // Replace 'yourfile.json' with the path to your JSON file
+//     .then((response) => response.json())
+//     .then((data) => {
+//       displayQueryName(data);
+//     })
+//     .catch((error) => console.error("Error loading JSON:", error));
+// }
+// function writeFile(data) {
+
+// }
+// function displayQueryName(data) {
+//   queryNameList.innerHTML = "";
+//   data.forEach(function (mov, i) {
+//     const html = `<div class="Qyery_List">
+//                       <p>${mov["ID"]}: ${mov["Name"]}</p>
+//                     </div>`;
+//     queryNameList.insertAdjacentHTML("afterbegin", html);
+//   });
+// }
+// //Event listener//
+// pop.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   loadDataJson();
+//   writeFile();
+// });
